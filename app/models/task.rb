353436f9edd454e93dc4ecf1d540456d83130d75
Alexey Solovyev <alex.solovyev@gmail.com>
2021-03-16 15:33:57 +0400

    Add state transitions for Task

--- app/models/task.rb
+++ app/models/task.rb
@@ -6,4 +6,34 @@ class Task < ApplicationRecord
   validates :description, presence: true
   validates :author, presence: true
   validates :description, length: { maximum: 500 }
+
+  state_machine initial: :new_task do
+    event :cancel do
+      transition new_task: :archived
+    end
+    event :develop do
+      transition new_task: :in_development
+    end
+    event :test do
+      transition in_development: :in_qa
+    end
+    event :fixing do
+      transition in_qa: :in_development
+    end
+    event :review do
+      transition in_qa: :in_code_review
+    end
+    event :reject do
+      transition in_code_review: :in_development
+    end
+    event :approve do
+      transition in_code_review: :ready_for_release
+    end
+    event :release do
+      transition ready_for_release: :released
+    end
+    event :archive do
+      transition released: :archived
+    end
+  end
 end

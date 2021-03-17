75fcb02fbb0338443800236de045bd77d1c11a06
Alexey Solovyev <alex.solovyev@gmail.com>
2021-03-17 17:10:10 +0400

    Add controller and layout for user authentication

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   scope module: :web do
     resource :board, only: :show
+    resources :developers, only: [:new, :create]
     resource :session, only: [:new, :create, :destroy]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html

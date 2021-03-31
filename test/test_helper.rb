6a6bb4a8d2b94f578a2baf1c5d6ea9f3f447ebdd
Alexey Solovyev <alex.solovyev@gmail.com>
2021-03-31 16:48:41 +0400

    Add Coveralls

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,12 @@
+require 'simplecov'
+require 'coveralls'
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
+                                                                 SimpleCov::Formatter::HTMLFormatter,
+                                                                 Coveralls::SimpleCov::Formatter,
+                                                               ])
+SimpleCov.start('rails')
+
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
